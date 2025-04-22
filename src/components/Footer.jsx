f9cf5b631788f24dd31c4ddc5edca435fb62a38a
antonella1986 <antonella.leo.mail@gmail.com>
2025-04-22 17:06:35 +0200

    add dynamic year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <footer className="bg-dark text-light py-3">
             <p className="text-center">
-                Copyright &copy; Movies' reviews
+                Copyright &copy; {new Date().getFullYear()} Movies' reviews
             </p>
         </footer>
     )
